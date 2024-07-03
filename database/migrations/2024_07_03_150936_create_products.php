fd8effd2ff6b33e5dead6f03a64ae92c90a7e95e
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-07-04 00:38:15 +0600

    Database Product Table

--- database/migrations/2024_07_03_150936_create_products.php
+++ database/migrations/2024_07_03_150936_create_products.php
@@ -13,6 +13,24 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 200);
+            $table->string('short_des', 500);
+            $table->string('price', 50);
+            $table->boolean('discount');
+            $table->string('discount_price', 50);
+            $table->string('image', 200);
+            $table->float('star');
+            $table->enum('remark',['popular', 'new', 'special', 'trending', 'reuglar']);
+
+            // F-kye
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('brand_id');
+
+            // Relationship
+            $table->foreigne('category_id')->references('id')->on('categories')->restrictOnDelete()->cascadeOnUpdate();
+            $table->foreigne('brand_id')->references('id')->on('brands')->restrictOnDelete()->cascadeOnUpdate();
+
+
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();        });
     }
