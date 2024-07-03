80af4e595e1677a148fda5021ec211a4a5a1a004
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-07-04 01:01:13 +0600

    Database Slider Table

--- database/migrations/2024_07_03_150952_create_products_sliders.php
+++ database/migrations/2024_07_03_150952_create_products_sliders.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('products_sliders', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title', 200);
+            $table->string('short_des', 500);
+            $table->string('image', 200);
+
+            $table->unsignedBigInteger('product_id')->unique();
+
+            // Relationship
+            $table->foreigne('product_id')->references('id')->on('products')->restrictOnDelete()->cascadeOnUpdate();
+
+
+
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
