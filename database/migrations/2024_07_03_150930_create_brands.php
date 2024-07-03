b997f32c996ff24f8b2439d08f2f9e6c78c2fbb1
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-07-04 00:12:17 +0600

    Database Brand table

--- database/migrations/2024_07_03_150930_create_brands.php
+++ database/migrations/2024_07_03_150930_create_brands.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->string('brandName', 50);
+            $table->string('brandImg', 300);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();        });
     }
