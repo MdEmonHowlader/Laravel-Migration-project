0f37db37240d7917cd843a64d030b6663a70b16b
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-07-04 00:42:56 +0600

    Database category table

--- database/migrations/2024_07_03_150923_create_categories.php
+++ database/migrations/2024_07_03_150923_create_categories.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('categoryName', 50);
+            $table->string('categoryImg', 300);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
