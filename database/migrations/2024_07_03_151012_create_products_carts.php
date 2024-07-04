4b3fd00ee3da2614cc407edaa3ec74884d96dceb
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-07-04 23:34:49 +0600

    Database Product cart table

--- database/migrations/2024_07_03_151012_create_products_carts.php
+++ database/migrations/2024_07_03_151012_create_products_carts.php
@@ -14,6 +14,14 @@ return new class extends Migration
         Schema::create('products_carts', function (Blueprint $table) {
             $table->id();
 
+            $table->string('email', 50);
+            $table->unsignedBigInteger('product_id');
+            $table->string('color', 200);
+            $table->string('size', 200);
+            $table->string('qty', 200);
+
+            $table->foreigne('product_id')->references('id')->on('products')->restrictOnDelete()->cascadeOnUpdate();
+            $table->foreigne('email')->references('email')->on('profiles')->restrictOnDelete()->cascadeOnUpdate();
 
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
